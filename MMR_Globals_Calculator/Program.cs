9a2fd78b28503571cb7b96a3f0456f70ca963a50
Bryan Morgan <bryan.morgan@infinityels.com>
2020-01-23 21:53:02 -0600

    DI config for EF Core

--- MMR_Globals_Calculator/Program.cs
+++ MMR_Globals_Calculator/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.IO;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration.Json;
+using MMR_Globals_Calculator.Database.HeroesProfile;
+using MMR_Globals_Calculator.Database.HeroesProfileBrawl;
+using MMR_Globals_Calculator.Database.HeroesProfileCache;
 using MMR_Globals_Calculator.Models;
 
 
@@ -31,13 +35,22 @@ namespace MMR_Globals_Calculator
             
             // Set up the objects we need to get to configuration settings
             var config = LoadConfiguration();
-            var a = config.GetSection("DbSettings");
             var dbSettings = config.GetSection("DbSettings").Get<DbSettings>();
-            
             // Add the config to our DI container for later use
             services.AddSingleton(config);
             services.AddSingleton(dbSettings);
-            
+
+            // EF Db config
+            services.AddDbContext<HeroesProfileContext>(options => options.UseMySql(ConnectionStringBuilder.BuildConnectionString(dbSettings)));
+            dbSettings.database = "heroesprofile_brawl";
+            services.AddDbContext<HeroesProfileBrawlContext>(options => options.UseMySql(ConnectionStringBuilder.BuildConnectionString(dbSettings)));
+            dbSettings.database = "heroesprofile_cache";
+            services.AddDbContext<HeroesProfileCacheContext>(options => options.UseMySql(ConnectionStringBuilder.BuildConnectionString(dbSettings)));
+            dbSettings.database = "heroesprofile";
+
+            services.AddScoped<RunMmrService>();
+            services.AddScoped<MmrCalculatorService>();
+
             // IMPORTANT! Register our application entry point
             services.AddTransient<ConsoleApp>();
             return services;
